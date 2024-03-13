0fbe55a08d03eace4021bc20ac4c6ac335b6c695
anborowc <anborowc@cisco.com>
2024-03-13 20:53:17 +0000

    - deleted pictures db, since we don't keep them in MongoDB; - updated plant_entries schema as per the diagram - edited the pages a bit

--- README.md
+++ README.md
@@ -8,5 +8,6 @@
 2.  npm i body-parser
 3.  npm i socket.io
 4.  npm i mongoose
+5.  npm i multer
 
 
--- server-side/controllers/plant_entries.js
+++ server-side/controllers/plant_entries.js
@@ -3,13 +3,21 @@ const plantEntryModel = require('../models/plant_entries');
 // Function to create new plant entries
 exports.create = function (plantData, filePath) {
     let plant_entry = new plantEntryModel({
+        username: plantData.username,
+        plant_name: plantData.plant_name,
         image: filePath,
         location: plantData.location,
         description: plantData.description,
         height: plantData.height,
         spread: plantData.spread,
-        plant_name: plantData.plant_name,
-        username: plantData.username,
+        flowers: plantData.flowers,
+        colour_flowers: plantData.colour_flowers,
+        leaves: plantData.leaves,
+        fruits_seeds: plantData.fruits_seeds,
+        sun_exposure: plantData.sun_exposure,
+        identification_status: false,
+        dbpedia_URI: null,
+        date: new Date()
     });
 
     return plant_entry.save().then(plantEntry => {
