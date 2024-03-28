d6616e96053f572f0cdd27ac021517397a439e2b
anborowc <anborowc@cisco.com>
2024-03-28 10:22:01 +0000

    showing of images works!

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('./public/images/uploads/', express.static(path.join(__dirname, './public/images/uploads/')));
+app.use('images/uploads/', express.static(path.join(__dirname, 'images/uploads/')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,15 @@ const comments = require("../controllers/comments");
 var multer = require("multer");
 
 var storage = multer.diskStorage({
-  destination: '../public/images/uploads/',
-  filename: function (req, file, cb) {
-      var original = file.originalname;
-      var file_extension = original.split(".");
-      filename = Date.now() + '.' + file_extension[file_extension.length - 1];
-      cb(null, filename);
-  }
+    destination :function(req, file, cb){
+        cb(null, '../public/images/uploads/');
+    },
+    filename: function (req, file, cb) {
+        var original = file.originalname;
+        var file_extension = original.split(".");
+        filename = Date.now() + '.' + file_extension[file_extension.length - 1];
+        cb(null, filename);
+    }
 });
 let upload = multer({ storage: storage });
 
