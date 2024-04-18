95d827a7161d3678eb81879f5d73bb334b38b1e5
anborowc <anborowc@cisco.com>
2024-04-18 10:21:45 +0100

    changed onload

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,18 +1,13 @@
 window.onload= (event) => {
-    sortBySavedValue()
+    sortPlants()
 }
 function sortPlants(data) {
     var sortValue = document.getElementById('sort-select').value;
-    localStorage.setItem('sortValue', sortValue)
-//     TODO here sort the data
-}
-
-function sortBySavedValue() {
-    let sortValue = getSortOrder()
     if(sortValue === undefined){
         sortValue = 'date-desc'
-        localStorage.setItem('sortValue', sortValue)
     }
+    localStorage.setItem('sortValue', sortValue)
+//     TODO here sort the data
 }
 
 function  getSortOrder(){
