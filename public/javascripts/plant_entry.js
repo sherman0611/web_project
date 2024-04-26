66f40f6596cfd3b7ad191c02f93029b3760f30a3
RalucaCruceriu <rcruceriu1@sheffield.ac.uk>
2024-04-26 13:47:35 +0100

    fixed dbpedia not showing

--- public/javascripts/plant_entry.js
+++ public/javascripts/plant_entry.js
@@ -1,26 +1,11 @@
 let plant_id = null;
 let plant_name = null;
-const resource = `http://dbpedia.org/resource/${plant_name}`;
-const endpointUrl = 'https://dbpedia.org/sparql';
-const sparqlQuery = `
-                 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                 PREFIX dbo: <http://dbpedia.org/ontology/>
 
-                 SELECT ?label ?abstract ?link
-                 WHERE {
-                  <${resource}> dbo:abstract ?abstract .
-                  <${resource}> rdfs:label ?label .
-                  <${resource}> dbo:wikiPageID ?link .
-                 FILTER (langMatches(lang(?abstract), "EN"))
-            }`;
-
-const encodedQuery = encodeURIComponent(sparqlQuery);
-const url = `${endpointUrl}?query=${encodedQuery}&format=json`
 let map;
 
 window.onload = function () {
     plant_id = document.getElementById('plant-id').value;
-    plant_name = document.getElementsByTagName('h1')[0]
+    plant_name = document.getElementsByTagName('h1')[0].textContent;
     console.log(plant_name)
 
     socket.emit('join', plant_id);
@@ -38,7 +23,6 @@ window.onload = function () {
 }
 
 async function initMap() {
-    // The location of Uluru
 
     const mapElement = document.getElementById('map');
     if(mapElement !== null && mapElement.dataset !== null){
@@ -64,38 +48,51 @@ async function initMap() {
     }
 }
 
-function fetchDBPedia(){
+function fetchDBPedia() {
+
+    const resource = `http://dbpedia.org/resource/${plant_name}`;
+    const endpointUrl = 'https://dbpedia.org/sparql';
+    const sparqlQuery = `
+                 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                 PREFIX dbo: <http://dbpedia.org/ontology/>
+
+                 SELECT ?label ?abstract ?link
+                 WHERE {
+                  <${resource}> dbo:abstract ?abstract .
+                  <${resource}> rdfs:label ?label .
+                  <${resource}> dbo:wikiPageID ?link .
+                 FILTER (langMatches(lang(?abstract), "EN"))
+            }`;
+
+    const encodedQuery = encodeURIComponent(sparqlQuery);
+    const url = `${endpointUrl}?query=${encodedQuery}&format=json`;
+
     fetch(url)
         .then(response => response.json())
         .then(data => {
-
             let bindings = data.results.bindings;
             let result = JSON.stringify(bindings);
-            if(bindings[0]!==undefined){
-                let dbpTitle = document.getElementById('db_page_title');
-                dbpTitle.textContent = 'DBpedia Information';
+            let dbpTitle = document.getElementById('db_page_title');
+            dbpTitle.textContent = 'DBpedia Information';
 
-                let label = bindings[0].label.value;
-                let labelElement = document.getElementById('title_dbp')
-                labelElement.textContent = label;
-
-
-                let abstract = data.results.bindings[0].abstract.value;
-                let plantInfoElement = document.getElementById('abstract_dbp');
-                plantInfoElement.innerHTML = 'DBpedia Description: <br>' + abstract;
+            let label = bindings[0].label.value;
+            let labelElement = document.getElementById('title_dbp')
+            labelElement.textContent = label;
 
 
-                let link = bindings[0].link.value;
-                let linkElement = document.getElementById('link_dbp');
-                linkElement.textContent = 'More info';
-                linkElement.href = `http://dbpedia.org/page/${plant_name}`;
-            } else {
-                document.getElementById('plant_infoDbp').remove();
-            }
+            let abstract = data.results.bindings[0].abstract.value;
+            let plantInfoElement = document.getElementById('abstract_dbp');
+            plantInfoElement.innerHTML = abstract;
 
+
+            let link = bindings[0].link.value;
+            let linkElement = document.getElementById('link_dbp');
+            linkElement.textContent = 'More info';
+            linkElement.href = `http://dbpedia.org/page/${plant_name}`;
         })
         .catch(error => console.error('Error:', error));
 }
+
 
 // function setEqualColumnHeights() {
 //     let twoColumnContainers = document.getElementsByClassName('two-columns','container');
