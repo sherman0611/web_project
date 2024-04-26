02b67fd198e0e65bb8cd06801791eca4b3993f25
anborowc <anborowc@cisco.com>
2024-04-26 11:43:53 +0100

    working on plant display

--- public/javascripts/plant_entry.js
+++ public/javascripts/plant_entry.js
@@ -1,9 +1,27 @@
 let plant_id = null;
+let plant_name = null;
+const resource = `http://dbpedia.org/resource/${plant_name}`;
+const endpointUrl = 'https://dbpedia.org/sparql';
+const sparqlQuery = `
+                 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                 PREFIX dbo: <http://dbpedia.org/ontology/>
 
-let map
+                 SELECT ?label ?abstract ?link
+                 WHERE {
+                  <${resource}> dbo:abstract ?abstract .
+                  <${resource}> rdfs:label ?label .
+                  <${resource}> dbo:wikiPageID ?link .
+                 FILTER (langMatches(lang(?abstract), "EN"))
+            }`;
+
+const encodedQuery = encodeURIComponent(sparqlQuery);
+const url = `${endpointUrl}?query=${encodedQuery}&format=json`
+let map;
 
 window.onload = function () {
     plant_id = document.getElementById('plant-id').value;
+    plant_name = document.getElementsByTagName('h1')[0]
+    console.log(plant_name)
 
     socket.emit('join', plant_id);
 
@@ -15,6 +33,8 @@ window.onload = function () {
     usernameDefining();
     assignCommentAuthor();
     scrollToBottomChat();
+    fetchDBPedia();
+    initMap();
 }
 
 async function initMap() {
@@ -44,4 +64,50 @@ async function initMap() {
     }
 }
 
-document.onload = initMap()
+function fetchDBPedia(){
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+
+            let bindings = data.results.bindings;
+            let result = JSON.stringify(bindings);
+            if(bindings[0]!==undefined){
+                let dbpTitle = document.getElementById('db_page_title');
+                dbpTitle.textContent = 'DBpedia Information';
+
+                let label = bindings[0].label.value;
+                let labelElement = document.getElementById('title_dbp')
+                labelElement.textContent = label;
+
+
+                let abstract = data.results.bindings[0].abstract.value;
+                let plantInfoElement = document.getElementById('abstract_dbp');
+                plantInfoElement.innerHTML = 'DBpedia Description: <br>' + abstract;
+
+
+                let link = bindings[0].link.value;
+                let linkElement = document.getElementById('link_dbp');
+                linkElement.textContent = 'More info';
+                linkElement.href = `http://dbpedia.org/page/${plant_name}`;
+            } else {
+                document.getElementById('plant_infoDbp').remove();
+            }
+
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function setEqualColumnHeights() {
+    let twoColumnContainers = document.getElementsByClassName('two-columns','container');
+
+    for (let child_container of twoColumnContainers[0]) {
+        let firstColumn = child_container.querySelector(':first-child');
+        let secondColumn = child_container.querySelector(':last-child');
+        let thirdColumn =
+        let tallestHeight = Math.max(firstColumn.offsetHeight, secondColumn.offsetHeight);
+
+        container.style.height = tallestHeight + 'px'; // Set container height
+        firstColumn.style.height = tallestHeight + 'px'; // Set first column height (optional)
+        secondColumn.style.height = tallestHeight + 'px'; // Set second column height (optional)
+    }
+}
