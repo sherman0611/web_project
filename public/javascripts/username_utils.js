20a6e86a827683fdbba7dfd4b5988a00edbb7688
anborowc <anborowc@cisco.com>
2024-05-02 10:21:29 +0100

    cleaned up username_utils; also, fixed username adding too

--- public/javascripts/username_utils.js
+++ public/javascripts/username_utils.js
@@ -6,7 +6,7 @@ function setUsername(){
 function getUsername() {
     let username = localStorage.getItem("username");
     if (!username) {
-        username = "anon";
+        username = "";
     }
     return username;
 }
@@ -16,5 +16,32 @@ function goToReferrer(){
         document.location = "/home"
     } else {
         document.location = document.referrer
+    }
+}
+
+function enterUsernameField() {
+    let html_to_insert = '<input class = "text-input" value = "'+getUsername()+'" type = "text" id = "username" name="username" >'
+    document.getElementById("enter_username_field").insertAdjacentHTML('beforeend',html_to_insert)
+}
+
+function usernameDefining(){
+    let html_to_insert = ""
+    var gotUsername = getUsername();
+    if(getUsername() === "") {
+        html_to_insert = '<input class="text_input" type="text" id="username" name="username">'+
+            '<button class="form-button" type="button" id="save-username" onclick="setUsername()">Save your username</button>'
+        // '<button class="form-button" id="save-username" onclick="preventDefault();stopPropagation();saveUsername()">Save your username</button>'
+    } else {
+        html_to_insert = '<input type="text" class="input-disabled" id="username" name="username" value='+gotUsername+' readonly><br>' +
+            '<a class="form-button" href="/enter_username">Change your username</a>'
+    }
+    document.getElementById("username-container").insertAdjacentHTML('beforeend',html_to_insert)
+}
+
+function identifyAuthor(){
+    if(getUsername() === document.getElementById("plant_author").innerText){
+        let plant_id = document.getElementById("plant_id").value
+        let html_to_insert = '<a class="form-button" href="/edit_plant/'+plant_id+'">Edit your plant entry</a>'
+        document.getElementsByClassName("nav-links")[0].insertAdjacentHTML("beforeend", html_to_insert)
     }
 }
