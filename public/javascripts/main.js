09103571767eaf30569efae28bb9ec2da6ae5156
sherman0611 <sherman0611@gmail.com>
2024-05-15 18:49:50 +0100

    minor ui changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -29,11 +29,14 @@ if ('serviceWorker' in navigator) {
 
 // Check if the browser supports the Notification API
 if ("Notification" in window) {
-    // Check if the user has granted permission to receive notifications
-    if (Notification.permission !== "denied") {
+    if (Notification.permission === "granted") {
+        // Notifications are allowed, you can proceed to create notifications
+        // Or do whatever you need to do with notifications
+    } else if (Notification.permission !== "denied") {
         // If the user hasn't been asked yet or has previously denied permission,
         // you can request permission from the user
         Notification.requestPermission().then(function (permission) {
+            // If the user grants permission, you can proceed to create notifications
             if (permission === "granted") {
                 navigator.serviceWorker.ready
                     .then(function (sw) {
