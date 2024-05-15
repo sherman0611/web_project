78716bd6ff5c44326fb139a0789602ab020b8043
RalucaCruceriu <rcruceriu1@sheffield.ac.uk>
2024-05-15 10:09:35 +0100

    collapsible tab for dbpedia data in progress

--- public/javascripts/view_plant.js
+++ public/javascripts/view_plant.js
@@ -27,7 +27,21 @@ window.onload = function () {
     scrollToBottomChat();
     // Map
     initMap();
+
+    var coll = document.getElementsByClassName("collapsible");
+    for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
 }
+
 
 // Initialising the map element if a given plant was using the coordinates
 async function initMap() {
