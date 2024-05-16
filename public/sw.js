0d92765f424f919638d49b545a6a015aa3c8487a
RalucaCruceriu <rcruceriu1@sheffield.ac.uk>
2024-05-16 18:20:54 +0100

    view_plant works offline if visited while online

--- public/sw.js
+++ public/sw.js
@@ -60,13 +60,46 @@ self.addEventListener('activate', event => {
     );
 })
 
-self.addEventListener('fetch', function(event) {
-    console.log('Service Worker: Fetch', event.request.url);
+// self.addEventListener('fetch', function(event) {
+//     console.log('Service Worker: Fetch', event.request.url);
+//
+//     event.respondWith(
+//         caches.match(event.request).then(function(response) {
+//             return response || fetch(event.request);
+//         })
+//     );
+// });
 
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then(function(response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(
+                    function(response) {
+                        // Check if we received a valid response
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // IMPORTANT: Clone the response. A response is a stream
+                        // and because we want the browser to consume the response
+                        // as well as the cache consuming the response, we need
+                        // to clone it so we have two streams.
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function(cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
     );
 });
 
@@ -101,6 +134,8 @@ self.addEventListener('sync', event => {
                     appendIfDefined(formData, 'status', syncEntry.formData.status);
                     appendIfDefined(formData, 'date_seen', syncEntry.formData.date_seen);
                     appendIfDefined(formData, 'time_seen', syncEntry.formData.time_seen);
+                    appendIfDefined(formData, 'date_post', new Date());
+                    appendIfDefined(formData, 'time_post', new Date().toTimeString().split(' ')[0]);
                     if (syncEntry.formData.image) {
                         appendIfDefined(formData, 'image', new File([syncEntry.formData.image], syncEntry.formData.image.name, {type: syncEntry.formData.image.type}));
                     }
